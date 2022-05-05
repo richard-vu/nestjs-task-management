512a2defd2bd087de4d9d890868fd90a5eb49c86
Richard Vu <richardvu.work@gmail.com>
2022-05-05 20:30:23 +0700

    Update Mock API

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
-import { TasksController } from './tasks.controller';
 
 @Module({
   controllers: [TasksController],
