bd2f2da8de0aa63f981cae27d0ed47b39bdda6e4
Richard Vu <richardvu.work@gmail.com>
2022-05-09 16:17:52 +0700

    Update

--- src/tasks/tasks.repository.ts
+++
@@ -1,8 +0,0 @@
-import { EntityRepository, Repository } from 'typeorm';
-import { TaskStatus } from './task-status.enum';
-import { Task } from './Model/task.entity';
-
-@EntityRepository(Task)
-export class TasksRepository extends Repository<Task> {
-
-}
